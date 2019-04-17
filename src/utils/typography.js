1db71ddffb0ae2223e5341eded2cc38ad5165012
Paul Peria <paul@cynder.io>
2019-04-18 04:41:47 +0800

    Updates

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-import moragaTheme from "typography-theme-moraga";
-moragaTheme.headerWeight = 400;
-const typography = new Typography(moragaTheme);
-
-export const { scale, rhythm, options } = typography;
-export default typography;
